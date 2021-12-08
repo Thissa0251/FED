162c7284c1276f2b64ffb1c069ea5490c49e549c
Thissa0251 <94459974+Thissa0251@users.noreply.github.com>
2021-12-08 12:57:04 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
 // JavaScript Document
+
+
+var deButton = document.querySelector("header section:first-of-type button");
+
+deButton.addEventListener("click", toggleMenu);
+
+function toggleMenu(event) {
+  var deNav = document.querySelector("nav");
+  deNav.classList.toggle("toonMenu");
+}
