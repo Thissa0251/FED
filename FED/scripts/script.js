af9c04633b0d17aa3c12921ae68aa0e4aa3ed703
Thissa0251 <94459974+Thissa0251@users.noreply.github.com>
2022-01-26 18:47:34 +0100

    Add files via upload

--- FED/scripts/script.js
+++ FED/scripts/script.js
@@ -1,1 +1,27 @@
 // JavaScript Document
+
+//Hamburger menu tonen en niet meer tonen*********************//
+
+var deButton = document.querySelector("header section:first-of-type button");
+
+deButton.addEventListener("click", toggleMenu);
+
+function toggleMenu(event) {
+  var deNav = document.querySelector("nav");
+  deNav.classList.toggle("toonMenu");
+}
+
+//dark modus inschakelen***************************************//
+
+var darkbox = document.querySelector("footer input");
+    darkbox.addEventListener('change', hetWordtDonker);
+
+function hetWordtDonker() {
+  if ( darkbox.checked ) {
+    document.documentElement.classList.add("darkmode");
+  }
+  else {
+    document.documentElement.classList.remove("darkmode");
+  }
+
+}    
